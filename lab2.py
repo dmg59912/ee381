9651a60e7d9a4f81f557a1bb7f5d4ecb8c6dcbef
dmg59912 <dmg138380@yahoo.com>
2020-09-30 19:12:33 -0700

    number 1 done

--- lab2.py
+++ lab2.py
@@ -6,7 +6,7 @@ import string
 
 ##### use probablity p:0.6; e0 = 0.05; e1 = 0.03  #######
 
-# 1) funtion for an n sided die
+# 1) Probability of erroneous transmission 
 def nSidedDie(p):
     n = np.size(p)
     cs = np.cumsum(p)
@@ -19,6 +19,7 @@ def nSidedDie(p):
     return d
 #
 
+# 2) Conditional probability: P(R=1|S=1)
 
 
 
@@ -26,19 +27,31 @@ def nSidedDie(p):
 #####################################################################################################
 def main(): 
     # problem 1
+    #N = 100000
+    N = 100
+    success = 0
     p0 = np.array([0.6, 0.4])
     e1 = np.array([0.03,0.97])
     e0 = np.array([0.95,0.05])
-    s = nSidedDie(p0)
-    s = s -1 
-    if s == 1:
-        r = nSidedDie(e1)
-        r = r-1
-    else:
-        r = nSidedDie(e0)
-        r = r-1
+    
+    for i in range(0,N):
+        s = nSidedDie(p0)
+        s = s -1 
+        if s == 1:
+            r = nSidedDie(e1)
+            r = r-1
+        else:
+            r = nSidedDie(e0)
+            r = r-1
 
-    print("S is ", s, " and R is " , r)
+        if s == r:
+            success +=1
+
+    print("success ", success)
+    p_success = success/N
+    q_failure = 1 - p_success
+    print("P of success ", p_success)
+    print("P of failure ", round(q_failure,2))
     # problem 2
 
 
