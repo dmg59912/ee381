9bfc9e39828c423ccda38acdcf430acfcb1c936c
dmg59912 <dmg138380@yahoo.com>
2020-11-18 20:09:28 -0800

    starting lab 5"

---
+++ lab5.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import random
+import string
+import math
+
+
+# Use the following numbers 
+# Total number of bearings: N=1,500,000
+# • Population mean: μ = 55  grams;
+# • Population standard deviation: σ= 5  grams;
+# • Sample sizes: n= 1, 2, ... 200
+
+
+def main(): 
+    print('hello')
+#########################################################################################################        
+if __name__ == '__main__':
+    main()
+  
